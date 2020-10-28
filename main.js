099dbf457b82bb8d4e0da1ee1cca27f11e76daff
davidpa7710 <72769538+davidpa7710@users.noreply.github.com>
2020-10-28 16:19:53 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+document.querySelector(".content-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+  
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.new-cards', { delay: 300 });
+ScrollReveal().reveal('.banners-one', { delay: 300 });
+ScrollReveal().reveal('.banners-two', { delay: 300 });
